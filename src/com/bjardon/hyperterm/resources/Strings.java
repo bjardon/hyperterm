42b2972911816a02882fe2e0c780882c94eb4c84
Bruno Jardon <bjardon97@gmail.com>
2018-12-12 15:24:34 -0600

    Added application icon and internationalization strings

---
+++ src/com/bjardon/hyperterm/resources/Strings.java
@@ -0,0 +1,20 @@
+package com.bjardon.hyperterm.resources;
+
+public class Strings {
+
+    public static class Errors {
+        public static String ERROR_OCCURRED = "Ocurrió un error";
+        public static String DEVICE_NOT_CONNECTED = "No hay dispositivos disponibles";
+        public static String DEVICE_NOT_CONNECTED_DESCRIPTION = "No se encontró ningún dispositivo serial conectado al equipo. Por favor verifique sus conexiones y pruebe de nuevo.";
+        public static String DEVICE_NOT_SELECTED = "No seleccionó dispositivo";
+        public static String DEVICE_NOT_SELECTED_DESCRIPTION = "No seleccionó ningún dispositivo serial de la lista. Por favor verifique su selección y pruebe de nuevo.";
+        public static String FAILED_CONNECTION = "Conexión fallida";
+        public static String FAILED_CONNECTION_DESCRIPTION = "No se pudo establecer comunicación con el dispositivo. Por favor pruebe de nuevo.";
+
+    }
+
+    public static class Notifications {
+        //public static String
+    }
+
+}
